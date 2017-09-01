5514959ed9eb8058ff26826809422f00ff42f883
Thomas Burleson <ThomasBurleson@Gmail.com>
2017-09-01 08:00:43 -0500

    fix(3): remove all subscribes, mergeMap
    * remove hack to show initial list with merge()
    * remove subscribe mergeMap()
    * assign to property directly
    * remove implements OnInit
    * delay showing fullPosts for 1 sec
    * show better imports of RxJS operators

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,33 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PostService, Post } from '../post.service';
 import { UserService } from '../user.service';
 
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
-  posts$: Observable<Array<PostWithAuthor>>;
+export class PostsComponent {y
+  posts$: Observable<Array<PostWithAuthor>> = this.loadPosts();
 
   constructor(private post: PostService, private user: UserService) {}
 
-  ngOnInit() {
-    // Show initial raw post immediately
-    const rawPosts$ = this.posts$ = this.post.fetch();
+  protected loadPosts() : Observable<Array<PostWithAuthor>> {
+    const rawPosts$ = this.post.fetch();
+    const loadFullPostFn = post => {
+      return this.user
+                 .getUser(post.userId)
+                 .map( user => ({...post, authorName:user.name}) );
+    };
 
-    rawPosts$.subscribe(posts => {
-      // Note: posts$ is a [ Observable<PostWithAuthor> ]
-      const posts$ : Array<Observable<PostWithAuthor>> = posts.map(post => {
-        return this.user
-                   .getUser(post.userId)
-                   .map( user => ({...post, authorName: user.name}));
-      });
+    return rawPosts$.mergeMap( posts => {
+      const collection$ = posts.map(loadFullPostFn);        // Array<Observables<PostWithAuthor>>
+      const fullPosts$  = Observable.forkJoin(collection$); // Observable<Array<PostWithAuthor>>
 
-      this.posts$ = Observable.forkJoin(posts$);
+      // 1st show raw posts, then later show the full posts
+      // NOTE: use 1-sec delay to show raw posts onscreen
+      return Observable.of(posts).merge(fullPosts$.delay(1000));
     });
 
   }

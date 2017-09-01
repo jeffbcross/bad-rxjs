2892e4f413c434fd51e2fe37e8563f16b43a0d4e
Thomas Burleson <ThomasBurleson@Gmail.com>
2017-09-01 07:40:34 -0500

    fix(0): expand 'bad' logic for easy consumption
    * refactorings will be done next to eliminate the 'badness'

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService, Post } from '../post.service';
 import { UserService } from '../user.service';
+
 import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-posts',
@@ -13,16 +15,22 @@ export class PostsComponent implements OnInit {
   constructor(private post: PostService, private user: UserService) {}
 
   ngOnInit() {
-    this.post
-      .fetch()
-      .subscribe(posts => {
-        Observable.forkJoin(posts
-            .map(post => this.user.getUser(post.userId)
-              .map(user => ({...post, authorName: user.name}))))
-          .subscribe(postsWithAuthor => {
-            this.posts = postsWithAuthor;
-          });
+    const rawPosts$ = this.post.fetch();
+
+    rawPosts$.subscribe(posts => {
+      // Note: posts$ is a [ Observable<PostWithAuthor> ]
+      const posts$ : Array<Observable<PostWithAuthor>> = posts.map(post => {
+        return this.user
+                   .getUser(post.userId)
+                   .map( user => ({...post, authorName: user.name}));
       });
+
+      Observable.forkJoin(posts$).subscribe(list => {
+        // Assign to update template using *ngFor
+        this.posts = list
+      });
+    });
+
   }
 }
 
